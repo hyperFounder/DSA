e484052cfcfd07c53b9f9dd9132a5a04c48a8999
Ryan Ribeiro <117395661+hyperFounder@users.noreply.github.com>
2026-09-03 11:46:13 +0100

    Update TopKFrequentElements.java

--- AdvancedSorting/code/TopKFrequentElements.java
+++ AdvancedSorting/code/TopKFrequentElements.java
@@ -4,6 +4,36 @@ import java.util.*;
 
 public class TopKFrequentElements {
 
+    public static int[] topKFrequentTwo(int[] arr, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0;  i<arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        List<Integer> sortedByVal = map.entrySet()
+                .stream()
+                .sorted(new Comparator<Map.Entry<Integer, Integer>>() {
+                    @Override
+                    public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                        // Reverse order
+                        if (o1.getValue() > o2.getValue()){
+                            return -1;
+                        } else if (o1.getValue() < o2.getValue()){
+                            return 1;
+                        } else{
+                            return 0;
+                        }
+                    }
+                })
+                .map(Map.Entry::getKey)
+                .limit(k)
+                .collect(Collectors.toList());
+        System.out.println(sortedByVal);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println("key: " + entry.getKey());
+            System.out.println("value: " + entry.getValue());
+        }
+        return sortedByVal.stream().mapToInt(i->i).toArray();
+    }
 
     public static int[] topKFrequent(int[] nums, int k){
         List<Integer> bucket = new ArrayList<>();
