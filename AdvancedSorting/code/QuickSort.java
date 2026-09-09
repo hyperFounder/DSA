f2a4fe269ac8f1b67200f2620f910bb6b85a53c4
Ryan Ribeiro <117395661+hyperFounder@users.noreply.github.com>
2026-09-09 12:18:17 +0100

    Update QuickSort.java

--- AdvancedSorting/code/QuickSort.java
+++ AdvancedSorting/code/QuickSort.java
@@ -1,51 +1,41 @@
-public class QuickSort {
+package main.sorting;
+
+public class QuickSort{
 
     public static void main(String[] args) {
-        int[] data = {70, 30, 80, 90, 40, 50, 10};
+        int[] data = {70, 30, 80, 90, 40 , 50 , 10};
         System.out.println("Original: " + java.util.Arrays.toString(data));
         sort(data);
         System.out.println("Sorted: " + java.util.Arrays.toString(data));
-        // Output: [10, 30, 40, 50, 70, 80, 90]
-
     }
 
     public static void sort(int[] arr){
         if (arr == null || arr.length <= 1) return;
-        quickSort(arr, 0, arr.length-1);
+        quickSort(arr, 0, arr.length - 1);
     }
-
-    // Recursive divide and Conquer step
-    private static void quickSort(int[] array, int low, int high){
+    public static void quickSort(int[] arr, int low, int high){
         if (low < high){
             // Partition the array around a pivot
-            int pivotIndex = partition(array, low, high);
-            quickSort(array, low, pivotIndex - 1);
-            quickSort(array, pivotIndex + 1, high);
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex -1);
+            quickSort(arr, pivotIndex + 1, high);
         }
     }
 
-    private static int partition(int[] array, int low, int high){
-
-        int pivot = array[low];
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
         int i = low;
-
         for (int j = low + 1; j<=high; j++){
-
-            // If curr element is smaller than or equal to pivot
-            if (array[j] <= pivot){
+            if (arr[j] <= pivot){
                 // swap
                 i++;
-                swap(array, i, j);
+                swap(arr, i, j);
             }
         }
-        // Swap the pivot element which is sitting at 'low' into its correct position 'i'
-        swap(array, low, i);
+        swap(arr, low, i);
         return i;
     }
 
-    /**
-     * Helper method to swap two elements in the array.
-     */
     public static void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
